eca5bdc89799b73a81eec481fee3aed830b70ea3
hyperspace-worker <48484710+PWMaestro@users.noreply.github.com>
2024-09-17 22:28:54 +0300

    refactor: application startup process
    Add appsettings file with the correct name. Now default host builder can use it by default;
    Add launch settings file for launching file in Dev environment locally;
    Copy all appsettings.json files to output directory on build;

--- DWBot/Program.cs
+++ DWBot/Program.cs
@@ -2,16 +2,11 @@
 using DWBot.Infrastructure.Extensions;
 using DWBot.Models;
 using DWBot.Services;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Telegram.Bot;
 
 IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration((context, configBuilder) =>
-    {
-        configBuilder.AddJsonFile("appsetting.json");
-    })
     .ConfigureServices((context, services) =>
     {
         services.Configure<BotConfiguration>(
